c44072f58a927db086e7058e45dfc3e0b61eb6d1
qiukun <qiukun@hftu.edu.cn>
2022-04-14 07:20:02 -0700

    I can read the docker memory.stat files now.

--- dataDispose/main.py
+++ dataDispose/main.py
@@ -15,11 +15,21 @@ class Metric():
         self.inactive_anon_rate =0
         self.cpu_usage = 0
 
+    def toPrint(self):
+        return "{0:.2f}\t{1:.2f}\t{2:.2f}\t{3:.2f}\t{4:.2f}\t{5:.2f}\t{6:.2f}".format(self.rss, self.pgpgin_rate, self.pgpgout_rate,
+                                          self.pgfault_rate, self.pgmajfault_rate,
+                                          self.inactive_anon_rate, self.active_anon_rate)
+
+
 class Action():
     def __init__(self, action):
         self.action = action
         self.metric = Metric()
         self.failure_time = 0
+
+    def toPrint(self):
+        action_name = "{}\t{}".format(self.action[0], self.action[1])
+        return "{0}\t{1}\t{2:.2f}".format(action_name, self.metric.toPrint(), self.failure_time)
 
 class myParser():
     def __init__(self):
@@ -72,15 +82,26 @@ class myParser():
             except:
                 print("Fail to parse memory metrics in log file {}".format(fd.name))
                 exit(-1)
-        action.metric.rss = sum(rss_l)/len(rss_l)  # I am calculating the average.
+        action.metric.rss = self._get_average(rss_l, "rss", fd.name)  # I am calculating the average.
         action.metric.pgpgin_rate = self._get_average_rate(pgpgin_l, "pgpgin", fd.name) # since the pgpgin is accumulating, so I want to calculated the average rate.
-        action.metric.pgpgout_rate = self._get_average_rate(pgpgout_l, "pgpgout", fd.name) # since the pgpgin is accumulating, so I want to calculated the average rate.
-        action.metric.pgfault_rate = self._get_average_rate(pgfault_l, "pgfault", fd.name) # since the pgpgin is accumulating, so I want to calculated the average rate.
-        action.metric.pgmajfault_rate = self._get_average_rate(pgmajfault_l, "pgmajfault", fd.name) # since the pgpgin is accumulating, so I want to calculated the average rate.
-        action.metric.inactive_anon_rate = self._get_average_rate(pgmajfault_l, "pgmajfault", fd.name) # since the pgpgin is accumulating, so I want to calculated the average rate.
+        action.metric.pgpgout_rate = self._get_average_rate(pgpgout_l, "pgpgout", fd.name) # since the pgpgout is accumulating, so I want to calculated the average rate.
+        action.metric.pgfault_rate = self._get_average_rate(pgfault_l, "pgfault", fd.name) # since the pgfault is accumulating, so I want to calculated the average rate.
+        action.metric.pgmajfault_rate = self._get_average_rate(pgmajfault_l, "pgmajfault", fd.name) # since the pgmajfault is accumulating, so I want to calculated the average rate.
+        action.metric.inactive_anon_rate = self._get_average(inactive_anon_l, "inactive_anon", fd.name) # since the inactive_anon is accumulating, so I want to calculated the average.
+        action.metric.active_anon_rate = self._get_average(active_anon_l, "active_anon", fd.name) # since the active_anon is accumulating, so I want to calculated the average.
+
+    def _get_average(self, t_list, tag, fd_name):
+        try:
+            return sum(t_list) /len(t_list)
+        except:
+            print("There is no sufficient data when parse {} in file {}".format(tag, fd_name))
+            exit(-1)
 
     def _get_average_rate(self, t_list, tag,  fd_name):
         dfs = []
+        if len(t_list) ==0:
+            print("There is no sufficient data when parse {} in file {}".format(tag, fd_name))
+            exit(-1)
         if len(t_list)==1:
             return t_list[0]
         for i in range(len(t_list) - 1):
@@ -91,7 +112,6 @@ class myParser():
             else:
                 dfs.append(df)
         return sum(dfs)/len(dfs)
-
 
     def get_actions(self):
         pattern = '^ft_(0\.[0-9]{1,2})_([0-9]{1,2})M\.log$'
@@ -110,5 +130,7 @@ if __name__ == '__main__':
     actions = [Action((0.1, 10))]
     for a in actions:
         parser.get_action_failure_time(a)
+        parser.get_action_mem_metrics(a)
+        print(a.toPrint())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
